88319b1b174109e1d98d795b519bcf809c0fedec
Jose Luis Alfaro <joseluis@fulljaus.com>
2022-05-16 19:25:39 -0300

    Initial Repo

--- src/modules/Geo/Controllers/GetZipCodeByCriteria.php
+++ src/modules/Geo/Controllers/GetZipCodeByCriteria.php
@@ -44,8 +44,6 @@ final class GetZipCodeByCriteria
 
         //if(Cache::has($zip_code)) {
             $query = Cache::get($zip_code);
-            dd($query);
-            exit(0);
         /*} else {
             $query = Settlement::with('city','city.entity')->where('zip_code', $zip_code)->get()->toArray();
             if(!count($query)){
